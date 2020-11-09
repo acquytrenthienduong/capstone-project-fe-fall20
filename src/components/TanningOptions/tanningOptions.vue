4f95ba4102028eddfe1cf6b04f317f98f6bcb61d
SoncmSE05989 <SoncmSE05989@fpt.edu.vn>
2020-11-09 14:13:50 +0700

    first_commit

---
+++ src/components/TanningOptions/tanningOptions.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="root">
+    <div class="title">TANNING OPTIONS HERE</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.root {
+  padding-top: 48px;
+  padding-bottom: 48px;
+  .title {
+    padding: 16px 0;
+    text-align: center;
+  }
+}
+</style>
