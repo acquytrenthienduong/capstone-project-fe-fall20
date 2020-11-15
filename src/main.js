804868662b97d738c9ae6169b12d9fe1539da777
SoncmSE05989 <SoncmSE05989@fpt.edu.vn>
2020-11-15 18:15:13 +0700

    add_carousel

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from '@/plugins/vuetify'
+import vuetify from "@/plugins/vuetify";
+import "swiper/css/swiper.css";
+import Vue from "vue";
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import App from "./App.vue";
+import router from "./router";
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
-
+Vue.use(VueAwesomeSwiper /* { default options with global component } */);
 new Vue({
   vuetify,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
