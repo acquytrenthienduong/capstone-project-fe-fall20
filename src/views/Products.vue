1cd76636a3171ff50d4d7fce7ec640def58cf043
ntt19981812 <tientainguyen.98@gmail.com>
2020-11-20 00:19:24 +0700

    fix ui product

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -7,12 +7,12 @@
       </div>
     </div>
     <v-container class="products py-10">
-      <div class="product-card-container" v-for="n in 5" :key="n">
+      <div class="product-card-container">
         <v-card class="product-card" max-width="400">
           <v-img
             class="white--text align-end"
             height="200px"
-            :src="`https://source.unsplash.com/random/1280x720?${n}`"
+            src="@/assets/lotions/FIESTASUNO_1.png"
           >
             <v-card-title>Top 10 Australian beaches</v-card-title>
           </v-img>
@@ -32,6 +32,138 @@
           </v-card-actions>
         </v-card>
       </div>
+
+      <div class="product-card-container">
+        <v-card class="product-card" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="@/assets/lotions/FIESTASUNO_2.png"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text> Share </v-btn>
+
+            <v-btn color="orange" text> Explore </v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
+
+      <div class="product-card-container">
+        <v-card class="product-card" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="@/assets/lotions/FIESTASUNO_3.png"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text> Share </v-btn>
+
+            <v-btn color="orange" text> Explore </v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
+
+      <div class="product-card-container">
+        <v-card class="product-card" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="@/assets/lotions/FIESTASUNO_4.png"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text> Share </v-btn>
+
+            <v-btn color="orange" text> Explore </v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
+
+      <div class="product-card-container">
+        <v-card class="product-card" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="@/assets/lotions/FiestaSun_3.png"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text> Share </v-btn>
+
+            <v-btn color="orange" text> Explore </v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
+
+      <div class="product-card-container">
+        <v-card class="product-card" max-width="400">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="@/assets/lotions/FIESTASUNO_5.png"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text> Share </v-btn>
+
+            <v-btn color="orange" text> Explore </v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
+
+      
     </v-container>
     <v-container class="py-10">
       <v-pagination v-model="page" :length="6"></v-pagination>
