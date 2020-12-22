1f6b0351e08ae4982b9c8b996ebea73547e10c0f
ntt19981812 <tientainguyen.98@gmail.com>
2020-12-22 07:24:46 +0700

    footer

--- src/components/Home/socials.vue
+++ src/components/Home/socials.vue
@@ -1,19 +1,60 @@
 <template>
   <div class="root">
-    <img src="@/assets/home/socials.png" alt="socials" />
+    <v-img src="@/assets/home/socials.png" alt="socials">
+      <div class="text-center">
+        <p class="social">Follow us</p>
+        <v-btn class="ma-2" text icon color="black" @click="facebookHandle">
+          <v-icon>mdi-facebook</v-icon>
+        </v-btn>
+        <v-btn class="ma-2" text icon color="black" @click="instaHandle">
+          <v-icon>mdi-instagram</v-icon>
+        </v-btn>
+        <v-btn class="ma-2" text icon color="black" @click="youtubeHandle">
+          <v-icon>mdi-youtube</v-icon>
+        </v-btn>
+      </div>
+    </v-img>
+    <div class="text">HAHA</div>
   </div>
 </template>
 
 <script>
-export default { name: "home-socials" };
+export default {
+  name: "home-socials",
+  methods: {
+    facebookHandle() {
+      window.open("https://www.facebook.com/navatan.studio", "_blank");
+    },
+    instaHandle() {
+      window.open("https://www.instagram.com/navatan.studio", "_blank");
+    },
+    youtubeHandle() {
+      window.open("https://youtube.com", "_blank");
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
 .root {
+  .socialShare {
+    background-color: transparent;
+  }
+
   img {
     width: 100%;
     object-fit: contain;
     object-position: center;
     display: block;
   }
+}
+
+.social {
+  font-family: "Dancing Script";
+  font-size: 252%;
+  margin-bottom: -1%;
+}
+
+.text-center {
+  margin: 8%;
 }
 </style>
