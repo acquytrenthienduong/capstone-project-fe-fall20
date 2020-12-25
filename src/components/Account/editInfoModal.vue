8cf0e9352d38f48e6b1016cf27c6e153835ef5c7
Your-Full-Name <anh.dinhhoangquang@gmail.com>
2020-12-25 21:43:23 +0700

    hello

--- src/components/Account/editInfoModal.vue
+++ src/components/Account/editInfoModal.vue
@@ -7,9 +7,7 @@
         </v-list-item-title>
       </template>
       <v-card>
-        <v-card-title class="title px-8 py-4">
-          THÔNG TIN KHÁCH HÀNG
-        </v-card-title>
+        <v-card-title class="title px-8 py-4"> THÔNG TIN KHÁCH HÀNG </v-card-title>
         <v-container class="px-8">
           <v-row>
             <v-col>
@@ -57,9 +55,7 @@
           <v-btn color="green darken-1" text @click="updateCustomer">
             LƯU THAY ĐỔI
           </v-btn>
-          <v-btn color="red darken-1" text @click="dialog = false">
-            THOÁT
-          </v-btn>
+          <v-btn color="red darken-1" text @click="dialog = false"> THOÁT </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -92,18 +88,12 @@
             ></v-text-field>
             <h3 v-if="checkEmpty">Hãy nhập đủ thông tin nhé!</h3>
             <h3 v-if="checkMatchPW">Xác nhận mật khẩu mới không đúng!</h3>
-            <h3 v-if="checkCurrentPassword">
-              Mật khẩu hiện tại của bạn không đúng!
-            </h3>
+            <h3 v-if="checkCurrentPassword">Mật khẩu hiện tại của bạn không đúng!</h3>
           </v-container>
           <v-card-actions class="px-10 pb-10">
             <v-spacer></v-spacer>
-            <v-btn color="red darken-1" text @click="reset">
-              Hủy
-            </v-btn>
-            <v-btn color="green darken-1" text @click="changePassdRule">
-              Lưu
-            </v-btn>
+            <v-btn color="red darken-1" text @click="reset"> Hủy </v-btn>
+            <v-btn color="green darken-1" text @click="changePassdRule"> Lưu </v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -114,19 +104,12 @@
         <v-card>
           <v-card-title class="title"> THAY ĐỔI SỐ ĐIỆN THOẠI </v-card-title>
           <v-container class="px-10 pt-10">
-            <v-text-field
-              label="Nhập số điện thoại mới"
-              outlined
-            ></v-text-field>
+            <v-text-field label="Nhập số điện thoại mới" outlined></v-text-field>
             <v-text-field outlined label="Nhập mật khẩu"></v-text-field>
           </v-container>
           <v-card-actions class="px-10 pb-10">
             <v-spacer></v-spacer>
-            <v-btn
-              color="black darken-1"
-              class="white--text"
-              @click="moveToStep2"
-            >
+            <v-btn color="black darken-1" class="white--text" @click="moveToStep2">
               Tiếp tục
             </v-btn>
             <v-btn color="red darken-1" text @click="changePhoneDialog = false">
@@ -146,18 +129,10 @@
           </v-container>
           <v-card-actions class="px-10 pb-10">
             <v-spacer></v-spacer>
-            <v-btn
-              color="black darken-1"
-              class="white--text"
-              @click="moveToStep2"
-            >
+            <v-btn color="black darken-1" class="white--text" @click="moveToStep2">
               Xác nhận
             </v-btn>
-            <v-btn
-              color="red darken-1"
-              text
-              @click="confirmPhoneDialog = false"
-            >
+            <v-btn color="red darken-1" text @click="confirmPhoneDialog = false">
               Hủy
             </v-btn>
           </v-card-actions>
@@ -206,29 +181,27 @@ export default {
     },
 
     updateCustomer() {
-      axios
-        .get(this.host + "/loadCustomer/" + this.customerId)
-        .then((response) => {
-          console.log("response", response);
-          var customer = response.data;
-          customer.name = this.name;
-          customer.email = this.email;
-          localStorage.setItem("customerName", customer.name);
-          localStorage.setItem("customerEmail", customer.email);
-          // localStorage.setItem("customerPhone", response.data.account);
-          axios
-            .post(this.host + "/updateProfile/" + this.customerId, {
-              customer,
-            })
-            .then((response) => {
-              console.log("res", response);
-              swal("Thành công", "Cập nhật thông tin thành công!", "success");
-              this.dialog = false;
-            })
-            .catch((e) => {
-              this.errors.push(e);
-            });
-        });
+      axios.get(this.host + "/loadCustomer/" + this.customerId).then((response) => {
+        console.log("response", response);
+        var customer = response.data;
+        customer.name = this.name;
+        customer.email = this.email;
+        localStorage.setItem("customerName", customer.name);
+        localStorage.setItem("customerEmail", customer.email);
+        // localStorage.setItem("customerPhone", response.data.account);
+        axios
+          .post(this.host + "/updateProfile/" + this.customerId, {
+            customer,
+          })
+          .then((response) => {
+            console.log("res", response);
+            swal("Thành công", "Cập nhật thông tin thành công!", "success");
+            this.dialog = false;
+          })
+          .catch((e) => {
+            this.errors.push(e);
+          });
+      });
     },
 
     reset() {
@@ -242,38 +215,25 @@ export default {
     },
 
     changePassdRule() {
-      if (
-        this.oldPassword != "" &&
-        this.newPassWord != "" &&
-        this.confimPassword != ""
-      ) {
+      if (this.oldPassword != "" && this.newPassWord != "" && this.confimPassword != "") {
         this.checkEmpty = false;
         if (this.newPassWord === this.confimPassword) {
           this.checkMatchPW = false;
           axios
-            .get(this.host + "/loadCustomer/" + this.customerId)
+            .post(this.host + "/changePassword/" + this.customerId, {
+              oldpassword: this.oldPassword,
+              newpassword: this.newPassWord,
+            })
             .then((response) => {
-              console.log("response", response);
-              if (response.data.password === this.oldPassword) {
-                var customer = response.data;
-                customer.password = this.newPassWord;
-                axios
-                  .post(this.host + `/updateProfile/` + this.customerId, {
-                    customer,
-                  })
-                  .then((response) => {
-                    console.log("res", response);
-                    swal("Thành công", "Đổi mật khẩu thành công!", "success");
-
-                    this.reset();
-                    this.changePasswordDialog = false;
-                  })
-                  .catch((e) => {
-                    this.errors.push(e);
-                  });
+              console.log(response);
+              if (response.status === 200) {
+                this.reset();
               } else {
                 this.checkCurrentPassword = true;
               }
+            })
+            .catch(() => {
+              this.checkCurrentPassword = true;
             });
         } else {
           this.checkMatchPW = true;
